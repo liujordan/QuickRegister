0f56a9e9d86c41d4d51839cf67b5278b31f950d1
Yong Cho Yin <me@techmastary.net>
2017-10-10 01:29:55 -0400

    add clubs app

--- quickregister/views.py
+++ quickregister/views.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
-from braces import views
+from braces.views import AnonymousRequiredMixin, FormValidMessageMixin, LoginRequiredMixin, MessageMixin
 from .forms import LoginForm, RegistrationForm
 from django.contrib.auth.models import User
 from django.views import generic
@@ -12,17 +12,21 @@ class HomePageView(generic.TemplateView):
     template_name = "_home.html"
 
 
-class SignUpView(generic.CreateView, views.AnonymousRequiredMixin, views.FormValidMessageMixin):
+class SignUpView(generic.CreateView, AnonymousRequiredMixin, FormValidMessageMixin):
+    # TODO: automatically sign in registered user
     form_class = RegistrationForm
+    success_url = reverse_lazy('signin')
     model = User
     template_name = 'accounts/signup.html'
+    # this doesn't show
     form_valid_message = "Successfully signed up."
 
 
-class LoginView(generic.FormView, views.AnonymousRequiredMixin, views.FormValidMessageMixin):
+class LoginView(generic.FormView, AnonymousRequiredMixin, FormValidMessageMixin):
     form_class = LoginForm
     success_url = reverse_lazy('home')
     template_name = 'accounts/login.html'
+    # this doesn't show
     form_valid_message = "You are now logged in."
 
     def form_valid(self, form):
@@ -37,7 +41,7 @@ class LoginView(generic.FormView, views.AnonymousRequiredMixin, views.FormValidM
             return self.form_invalid(form)
 
 
-class LogOutView(generic.RedirectView, views.LoginRequiredMixin, views.MessageMixin):
+class LogOutView(generic.RedirectView, LoginRequiredMixin, MessageMixin):
     url = reverse_lazy('home')
 
     def get(self, request, *args, **kwargs):
