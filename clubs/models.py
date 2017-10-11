1d08f2427b91f74937a491a5c02899aa74f742b2
Cho Yin Yong <choyin.yong@mail.utoronto.ca>
2017-10-10 23:04:59 -0400

    add user profile

--- clubs/models.py
+++ clubs/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.db.models import EmailField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Club(models.Model):
@@ -10,8 +14,34 @@ class Club(models.Model):
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
-        return str(self.club_name)
+    def __str__(self):
+        return self.club_name
 
     def get_absolute_url(self):
         return reverse('clubs.views.club', args=[self.slug])
+
+
+class Profile(models.Model):
+    """ Model for a profile of a user, storing additional details of a user """
+    # one to one relationship with a user model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # additional fields
+    email = EmailField(max_length=254)
+
+    # languages
+    primary_language = models.CharField(max_length=30)
+    secondary_language = models.CharField(max_length=30)
+
+    # resume
+    resume = models.FileField(upload_to='uploads/')
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
