1d89485977a83c2e074b0f2fc8f1ff8f63ac9509
Jordan Liu <liu.jordan.com@gmail.com>
2017-10-30 01:27:34 -0400

    use material design lite in Login html

--- quickregister/settings.py
+++ quickregister/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'profiles'
 ]
 
@@ -117,6 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATICFILES_DIRS = [
+    'C:/quick_register/static_files'
+]
 
 STATIC_URL = '/static/'
 
