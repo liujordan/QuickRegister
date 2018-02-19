4af0028734d02287fb34df70055e69b0f003d919
Jordan Liu <liu.jordan.com@gmail.com>
2018-02-18 22:00:26 -0500

    login and logout and home view

--- QuickRegister/urls.py
+++ QuickRegister/urls.py
@@ -19,8 +19,8 @@ from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-	path('', HomeView.as_view(), name='home')
+	path('', HomeView.as_view(), name='home'),
 	path('login/', auth_views.login, name='login'),
-	path('logout/', auth_views.logout, name='logout'),
+	path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('admin/', admin.site.urls),
 ]
