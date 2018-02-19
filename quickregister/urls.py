06d5e2436ce2fed53e12da0356aca90b0831ca8b
Jordan Liu <liu.jordan.com@gmail.com>
2018-02-18 21:30:44 -0500

    restart

--- quickregister/urls.py
+++ quickregister/urls.py
@@ -1,29 +1,21 @@
-"""quickregister URL Configuration
+"""QuickRegister URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-
-from quickregister.views import *
+from django.urls import path
 
 urlpatterns = [
-    url(r'^$', HomePageView.as_view(), name='home'),
-    # url(r'^', include('django.contrib.auth.urls'))
-    url(r'^admin/', admin.site.urls),
-    url(r'^login/$', redirect_if_logged_in, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'logout', 'next_page': '/'}, name='logout'),
-    url(r'^profiles/', include('profiles.urls')),
+    path('admin/', admin.site.urls),
 ]
