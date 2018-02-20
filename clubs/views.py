a405dd4871f6bd1174fb23d7838f38732580fd30
Jordan Liu <liu.jordan.com@gmail.com>
2018-02-20 18:18:20 -0500

    implement basic club profile

--- clubs/views.py
+++ clubs/views.py
@@ -9,13 +9,23 @@ from django.urls import reverse
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = 'clubs/join.html'
+    template_name = 'clubs/home.html'
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the books
         context['clubs'] = Club.objects.all()
+        return context
+
+class ClubView(TemplateView):
+    template_name = 'clubs/club.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['club'] = get_object_or_404(Club, pk=kwargs['pk'])
         return context
 
 class JoinView(View):
