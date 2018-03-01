9e0304197c170b37d6d28b111288eac573f76c92
Jordan Liu <liu.jordan.com@gmail.com>
2018-03-01 01:32:17 -0500

    complete CSV export for member information

--- clubs/urls.py
+++ clubs/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<int:pk>/join/', JoinView.as_view(), name='join'),
     path('<int:pk>/leave/', LeaveView.as_view(), name='leave'),
     path('<int:pk>/qr', QrView.as_view(), name='qr'),
+    path('<int:pk>/members', MembersView.as_view(), name='members'),
+    path('<int:pk>/members/export_csv', export_csv, name='export_csv'),
 ]
--- clubs/views.py
+++ clubs/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 import datetime
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from QuickRegister.models import Profile
 
 
 class HomeView(TemplateView):
@@ -30,6 +31,27 @@ class ClubView(TemplateView):
         context['club'] = get_object_or_404(Club, pk=kwargs['pk'])
         return context
 
+def export_csv(request, **kwargs):
+    if request.method == 'GET':
+        import csv
+        from django.utils.encoding import smart_str
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+        writer = csv.writer(response, csv.excel)
+        response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
+        fields = Profile._meta.get_fields()
+        club = get_object_or_404(Club, pk=kwargs['pk'])
+        # Write headers
+        writer.writerow([
+            smart_str(field.name) for field in fields
+        ])
+
+        # Write content
+        writer.writerow([
+            smart_str(getattr(x.user.profile, field.name)) for x in Membership.objects.filter(club=club) for field in fields 
+        ])
+        return response
+
 
 class JoinView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
@@ -43,6 +65,14 @@ class JoinView(LoginRequiredMixin, View):
             return HttpResponse("Successfully joined {}".format(club))
         return HttpResponse("You are already part of {}".format(club))
 
+class MembersView(LoginRequiredMixin, TemplateView):
+    template_name = 'clubs/members.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        club = get_object_or_404(Club, pk=kwargs['pk'])
+        context['members'] = [x.user for x in Membership.objects.filter(club=club)]
+        return context
 
 class QrView(TemplateView):
     template_name = 'clubs/qr.html'
@@ -59,3 +89,103 @@ class LeaveView(LoginRequiredMixin, View):
         if membership:
             membership.delete()
         return HttpResponseRedirect(reverse('clubs:home'))
+# for future reference..
+# def export_csv(modeladmin, request, queryset):
+#     import csv
+#     from django.utils.encoding import smart_str
+#     response = HttpResponse(content_type='text/csv')
+#     response['Content-Disposition'] = 'attachment; filename=mymodel.csv'
+#     writer = csv.writer(response, csv.excel)
+#     response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
+#     fields = Club._meta.get_fields()
+#     writer.writerow([
+#         smart_str(field.name) for field in fields
+#     ])
+#     for obj in queryset:
+#         writer.writerow([
+#             smart_str(obj._meta.get_field(field.name)) for field in fields
+#         ])
+#     return response
+# export_csv.short_description = u"Export CSV"
+
+# def export_xls(modeladmin, request, queryset):
+#     import xlwt
+#     response = HttpResponse(content_type='application/ms-excel')
+#     response['Content-Disposition'] = 'attachment; filename=mymodel.xls'
+#     wb = xlwt.Workbook(encoding='utf-8')
+#     ws = wb.add_sheet("MyModel")
+    
+#     row_num = 0
+    
+#     columns = [
+#         (u"ID", 2000),
+#         (u"Title", 6000),
+#         (u"Description", 8000),
+#     ]
+
+#     font_style = xlwt.XFStyle()
+#     font_style.font.bold = True
+
+#     for col_num in xrange(len(columns)):
+#         ws.write(row_num, col_num, columns[col_num][0], font_style)
+#         # set column width
+#         ws.col(col_num).width = columns[col_num][1]
+
+#     font_style = xlwt.XFStyle()
+#     font_style.alignment.wrap = 1
+    
+#     for obj in queryset:
+#         row_num += 1
+#         row = [
+#             obj.pk,
+#             obj.title,
+#             obj.description,
+#         ]
+#         for col_num in xrange(len(row)):
+#             ws.write(row_num, col_num, row[col_num], font_style)
+            
+#     wb.save(response)
+#     return response
+    
+# export_xls.short_description = u"Export XLS"
+
+# def export_xlsx(modeladmin, request, queryset):
+#     import openpyxl
+#     from openpyxl.cell import get_column_letter
+#     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+#     response['Content-Disposition'] = 'attachment; filename=mymodel.xlsx'
+#     wb = openpyxl.Workbook()
+#     ws = wb.get_active_sheet()
+#     ws.title = "MyModel"
+
+#     row_num = 0
+
+#     columns = [
+#         (u"ID", 15),
+#         (u"Title", 70),
+#         (u"Description", 70),
+#     ]
+
+#     for col_num in xrange(len(columns)):
+#         c = ws.cell(row=row_num + 1, column=col_num + 1)
+#         c.value = columns[col_num][0]
+#         c.style.font.bold = True
+#         # set column width
+#         ws.column_dimensions[get_column_letter(col_num+1)].width = columns[col_num][1]
+
+#     for obj in queryset:
+#         row_num += 1
+#         row = [
+#             obj.pk,
+#             obj.title,
+#             obj.description,
+#         ]
+#         for col_num in xrange(len(row)):
+#             c = ws.cell(row=row_num + 1, column=col_num + 1)
+#             c.value = row[col_num]
+#             c.style.alignment.wrap_text = True
+
+#     wb.save(response)
+#     return response
+
+# export_xlsx.short_description = u"Export XLSX"
